7b28be3615cefc5422dc0b30ea565f4beb3cdf64
Shuvo Paul <shuvopaul@yahoo.com>
2023-05-26 12:26:17 +0600

    chore: bill details style update

--- src/lib/components/InvoiceContacts.svelte
+++ src/lib/components/InvoiceContacts.svelte
@@ -4,7 +4,7 @@
 
 <div class="grid grid-cols-2 gap-x-8">
 	<div class="">
-		<h2>From</h2>
+		<h2 class="unstyled">From</h2>
 		<div class="grid grid-cols-1 gap-4">
 			<input type="text" placeholder="Business Name" bind:value={$billFrom.name}>
 			<input type="text" placeholder="Email" bind:value={$billFrom.email}>
@@ -13,7 +13,7 @@
 		</div>
 	</div>
 	<div class="">
-		<h2>Bill To</h2>
+		<h2 class="unstyled">Bill To</h2>
 		<div class="grid grid-cols-1 gap-4">
 			<input type="text" placeholder="Business Name" bind:value={$billTo.name}>
 			<input type="text" placeholder="Email" bind:value={$billTo.email}>
@@ -25,10 +25,10 @@
 
 <style lang="postcss">
 	h2 {
-		@apply mb-4;
+		@apply mb-4 text-lg font-semibold;
 	}
 
 	input {
-		@apply w-full;
+		@apply w-full border-0 bg-transparent;
 	}
 </style>
